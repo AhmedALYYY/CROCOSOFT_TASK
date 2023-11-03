3ab9bcbcbc63169dbdeb68b8c28d6ac2e386b2fc
Ahmed Aly <102419341+AhmedALYYY@users.noreply.github.com>
2023-11-03 11:23:15 +0200

    Add files via upload

---
+++ AhmedAly_Solution/app.py
@@ -0,0 +1,213 @@
+from flask import Flask, jsonify, request, abort
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+with app.app_context():
+    app.config.from_object('config.DevConfig')
+    mysql = MySQL(app)
+
+    # Creating a connection cursor
+    cursor = mysql.connection.cursor()
+
+    print("Connection cursor created")  # Debugging statement
+
+    # Executing SQL Statements
+    cursor.execute('''CREATE TABLE IF NOT EXISTS CUSTOMER (
+         CUSTOMER_ID	INTEGER NOT NULL AUTO_INCREMENT,
+         FIRST_NAME	VARCHAR(30) NOT NULL,
+         LAST_NAME	VARCHAR(30) NOT NULL,
+         EMAIL_ADDRESS	VARCHAR(64) NOT NULL,
+         TELEPHONE_NUMBER	CHAR(12) NOT NULL,
+          PRIMARY KEY	(CUSTOMER_ID))''')
+
+    print("Table CUSTOMER created")  # Debugging statement
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS VehicleType (
+         VehicleTypeID	INTEGER NOT NULL AUTO_INCREMENT,
+         NAME VARCHAR(30) NOT NULL,
+         CAPACITY INTEGER,
+          PRIMARY KEY (VehicleTypeID))''')
+
+    print("Table VehicleType created")  # Debugging statement
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS VEHICLE (
+         VEHICLE_ID	INTEGER NOT NULL AUTO_INCREMENT,
+         NAME VARCHAR(30) NOT NULL,
+         TypeID INTEGER,
+         PRICE decimal(6,2) NOT NULL,
+          PRIMARY KEY (VEHICLE_ID),
+          FOREIGN KEY (TypeID) REFERENCES VehicleType(VehicleTypeID))''')
+
+    print("Table VEHICLE created")  # Debugging statement
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS BOOKING (
+             BOOKING_ID	INTEGER NOT NULL AUTO_INCREMENT,
+             Customer_ID INTEGER,
+             Vehicle_ID INTEGER,
+             TOTAL_PRICE decimal(6,2) NOT NULL,
+             Booking_Date datetime DEFAULT current_timestamp,
+             Hiring_Date datetime DEFAULT current_timestamp,
+             Return_Date datetime Not NULL,
+              PRIMARY KEY (BOOKING_ID),
+              FOREIGN KEY (Customer_ID) REFERENCES CUSTOMER(CUSTOMER_ID),
+              FOREIGN KEY (Vehicle_ID) REFERENCES VEHICLE(VEHICLE_ID),
+              constraint check_dates check (Hiring_Date < Return_Date and DATEDIFF(Hiring_Date, Return_Date) <= 7 and DATEDIFF(Booking_Date, Hiring_Date) <= 7 ))''')
+
+    print("Table BOOKING created")  # Debugging statement
+
+    # Saving the Actions performed on the DB
+    mysql.connection.commit()
+
+    print("Changes committed to the database")  # Debugging statement
+
+    # Closing the cursor
+    cursor.close()
+
+    print("Connection cursor closed")  # Debugging statement
+
+
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    cursor = None
+    try:
+        # validate the received values
+        if request.method == 'POST':
+            _fname = request.form.get('first_name')
+            _lname = request.form.get('last_name')
+            _email = request.form.get('email')
+            _phone = request.form.get('phone_number')
+            if _fname and _email and _phone and _lname:
+
+                message = {
+                    'status': 200,
+                    'message': 'Customer added successfully',
+                }
+                resp = jsonify(message)
+                resp.status_code = 200
+                return resp
+            else:
+                abort(400)
+        else:
+            abort(404)
+    finally:
+        if cursor:
+            cursor.close()
+
+
+@app.route('/customers/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def updateCustomer(id):
+    cursor = None
+    try:
+        if request.method == 'GET':
+            cursor = mysql.connection.cursor()
+            cursor.execute("SELECT * FROM CUSTOMER WHERE CUSTOMER_ID=%s", str(id))
+            row = cursor.fetchone()
+            if row:
+                message = {
+                    'status': 200,
+                    'data': row,
+                }
+                resp = jsonify(message)
+                resp.status_code = 200
+                return resp
+            else:
+                message = {
+                    'status': 400,
+                    'message': 'Customer Not Found',
+                }
+                resp = jsonify(message)
+                resp.status_code = 400
+                return resp
+        elif request.method == 'DELETE':
+            cursor = mysql.connection.cursor()
+            cursor.execute("DELETE FROM CUSTOMER WHERE CUSTOMER_ID=%s", (str(id),))
+            mysql.connection.commit()
+            deleted_row_count = cursor.rowcount
+            if deleted_row_count > 0:
+                message = {
+                    'status': 200,
+                    'message': "Customer deleted successfully",
+                }
+                resp = jsonify(message)
+                resp.status_code = 200
+                return resp
+            else:
+                message = {
+                    'status': 400,
+                    'message': 'Customer Not Found',
+                }
+                resp = jsonify(message)
+                resp.status_code = 400
+                return resp
+        elif request.method == 'PUT':
+            _fname = request.form.get('first_name')
+            _lname = request.form.get('last_name')
+            _email = request.form.get('email')
+            _phone = request.form.get('phone_number')
+            if _fname and _email and _phone and _lname:
+                sql = "UPDATE CUSTOMER SET FIRST_NAME=%s, LAST_NAME=%s, EMAIL_ADDRESS=%s, TELEPHONE_NUMBER=%s " \
+                      "WHERE CUSTOMER_ID=%s"
+                data = (_fname, _lname, _email, _phone, id)
+                cursor = mysql.connection.cursor()
+                cursor.execute(sql, data)
+                mysql.connection.commit()
+                updated_row_count = cursor.rowcount
+                if updated_row_count > 0:
+                    message = {
+                        'status': 200,
+                        'message': "Customer update successfully",
+                    }
+                    resp = jsonify(message)
+                    resp.status_code = 200
+                    return resp
+                else:
+                    message = {
+                        'status': 400,
+                        'message': 'Customer Not Found',
+                    }
+                    resp = jsonify(message)
+                    resp.status_code = 400
+                    return resp
+            else:
+                abort(400)
+    finally:
+        if cursor:
+            cursor.close()
+
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    message = {
+        'status': 500,
+        'message': 'Server error',
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+    return resp
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    res = {'status': e.code,
+           'message': e.message if hasattr(e, 'message') else f'{e}'}
+    resp = jsonify(res)
+    resp.status_code = e.code
+    return resp
+
+
+
+
+if __name__ == '__main__':
+    app.run()
